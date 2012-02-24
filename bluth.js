0352c39c68793cd1912b8026efa151e10a8b147d
Gilmore Davidson <gilmoreorless@gmail.com>
2012-02-24 18:08:15 +1100

    Started hooking into a transport layer

--- bluth.js
+++ bluth.js
@@ -2,38 +2,54 @@
     var allowedMethods = {'get':1, 'post':1, 'put':1, 'delete':1}
     var bluthMethods = {}
     bluthMethods.get = function (params, callback) {
-        if (callback == null) {
-            if (typeof params == 'function') {
-                callback = params;
-                params = null;
-            }
+        if (arguments.length < 2 && typeof params == 'function') {
+            callback = params;
+            params = null;
         }
-        // TODO: Call transport
-        if (callback) {
-            callback(params);
-        }
+        // TODO: This needs to support custom transports
+        var transport = bluth.transports[bluth.defaultTransport],
+            url = this.getUrl();
+        return transport.send(url, 'get', null, params, callback);
+//        if (callback) {
+//            callback(params);
+//        }
     }
     function methodFactory(method) {
         return function (data, params, callback) {
-            callback(data, params);
+            if (arguments.length < 2 && typeof data == 'function') {
+                callback = data;
+                data = null;
+            } else if (arguments.length < 3 && typeof params == 'function') {
+                callback = params;
+                params = null;
+            }
+            var transport = bluth.transports[bluth.defaultTransport],
+                url = this.getUrl();
+            return transport.send(url, method, data, params, callback);
+//            callback(data, params);
         }
     }
     bluthMethods.post = methodFactory('post');
     bluthMethods.put = methodFactory('put');
-    bluthMethods['delete'] = methodFactory('delete');
+    bluthMethods.del = methodFactory('delete');
     
-    function bluthClient(serverUrl, transport) {
+    function BluthClient(serverUrl, transport) {
         this._serverUrl = serverUrl;
         this._transport = transport || bluth.defaultTransport;
     }
     
-    bluthClient.prototype.addPath = function (name, path, methods) {
-        this[name] = new bluthPath(path, methods);
+    BluthClient.prototype.addPath = function (name, path, methods) {
+        if (!name) {
+            throw TypeError('A name is required');
+        }
+        this[name] = new BluthPath(this, path, methods);
     }
     
-    function bluthPath(path, methods) {
+    function BluthPath(client, path, methods) {
         methods || (methods = ['get']);
-        this._path = path;
+        this._client = client;
+        this._path = path || '';
+        this._methods = methods;
         var self = this;
         methods.forEach(function (method) {
             method = method.toLowerCase();
@@ -43,8 +59,12 @@
         })
     }
     
+    BluthPath.prototype.getUrl = function () {
+        return this._client._serverUrl + this._path;
+    }
+    
     var bluth = global.bluth = function (serverUrl, transport) {
-        return new bluthClient(serverUrl, transport);
+        return new BluthClient(serverUrl, transport);
     }
     
     bluth.transports = {};
@@ -59,5 +79,12 @@
 (function (global, bluth, undefined) {
     bluth.addTransport('jquery', {
         // What goes here?
+        send: function (url, method, data, params, callback) {
+            // params?
+            var xhr = jQuery.ajax({
+                type: method,
+                data: data
+            }).success(callback)
+        }
     });
 })(this, this.bluth);
